0713118eb8ea5ca9cf24e508ddea90e4ec609122
sarahcabral <sarah.ly2009@gmail.com>
2020-11-16 19:38:28 -0300

    first commit

---
+++ src/main/java/com/example/projeto_af/ProjetoAfApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAfApplication.class, args);
+	}
+
+}
